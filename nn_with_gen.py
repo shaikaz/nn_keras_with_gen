82e09b0b56e0f840ca5348f8ce62746ae4bd32e2
Shai <shai@moonshot.co.il>
2016-05-15 17:24:39 +0300

    added metrices to model.compile

--- nn_with_gen.py
+++ nn_with_gen.py
@@ -232,6 +232,7 @@ model = Model(input = [globals()["input_{F}".format(F=field)] for field in sampl
                 output = output)
 
 model.compile(loss=loss_function,
+              metrics={'output': 'accuracy'},
               optimizer=optimizer)
 
 plot(model, to_file=path + dir_data + 'model.png', show_shapes=True)
@@ -285,7 +286,7 @@ def make_submission(test_dict, length_of_test_data, fname = "keras.csv"):
         for i in range(length_of_test_data):
             temp_id = test_dict['vt_id'][i]
             #temp_prob = np.float32(model.predict_on_batch(generate_sample(test_dict, i))[0][0])
-            temp_prob = np.float32(model.predict(generate_sample(test_dict, i), batch_size=1, verbose=0))
+            temp_prob = np.float32(model.predict(generate_sample(test_dict, i), batch_size=1, verbose=0)[0][0])
             temp_tr_val = test_dict['Greengeeks_clicks'][i]
             temp = [str(temp_id), temp_prob, str(temp_tr_val)]
             a.writerow(temp)
