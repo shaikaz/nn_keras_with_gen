31852d29438b7c1acc1d2810214f48bbc424ef5c
Shai <shai@moonshot.co.il>
2016-05-17 14:05:39 +0300

    try to create a predict function fix 2

--- nn_with_gen.py
+++ nn_with_gen.py
@@ -280,7 +280,7 @@ def generate_sample(data_dict, index):
             temp_batch_sample["input_{F}".format(F=field)] = temp_arr_sample
     return temp_batch_sample
 
-prob = K.function([model.input, K.learning_phase()], [model.output])
+prob = K.function([model.input], [model.output])
 def make_submission(test_dict, length_of_test_data, fname = "keras.csv"):
     with open(fname, 'wb') as f:
         a = csv.writer(f, delimiter=',')
@@ -289,7 +289,7 @@ def make_submission(test_dict, length_of_test_data, fname = "keras.csv"):
             temp_id = test_dict['vt_id'][i]
             #temp_prob = np.float32(model.predict_on_batch(generate_sample(test_dict, i))[0][0])
             #temp_prob = np.float32(model.predict(generate_sample(test_dict, i), batch_size=1, verbose=0)[0])
-            temp_prob = prob([generate_sample(test_dict, i), 0])
+            temp_prob = prob([generate_sample(test_dict, i)])
             temp_tr_val = test_dict['Greengeeks_clicks'][i]
             temp = [str(temp_id), temp_prob, str(temp_tr_val)]
             a.writerow(temp)
